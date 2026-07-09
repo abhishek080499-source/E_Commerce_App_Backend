6c6865021982e319c8655d09cf8d2dadf7a1b300
Abhishek Sharma <abhishek080499@gmail.com>
2026-07-09 14:51:33 +0530

    Cors update

--- server.js
+++ server.js
@@ -71,8 +71,6 @@ app.use((err, req, res, next) => {
 
 // ✅ Start server
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-}
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 module.exports = app;
